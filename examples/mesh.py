bd0bf7fb12dc522d24786bbbd87ac8d409ce5a6a
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2023-02-12 18:53:26 +0100

    Started the mesh visual (functional)

---
+++ examples/mesh.py
@@ -0,0 +1,40 @@
+# -----------------------------------------------------------------------------
+# Graphic Server Protocol (GSP)
+# Copyright 2023 Vispy Development Team - BSD 2 Clauses licence
+# -----------------------------------------------------------------------------
+# This example demonstrates usage of the JIT depth buffer. It is used with a
+# colormap to vary color according to the depth of a point (not to be confused
+# with the z coordinate).
+# -----------------------------------------------------------------------------
+import gsp
+import glm
+import meshio
+import numpy as np
+gsp.use("matplotlib")
+
+
+canvas = core.Canvas(512, 512, 100.0)
+viewport = core.Viewport(canvas, 0, 0, 512, 512)
+camera = glm.Camera("perspective", theta=10, phi=10)
+
+mesh = meshio.read("data/bunny-1024.obj")
+verts = 2*glm.fit_unit_cube(mesh.points).astype(np.float32)
+faces = mesh.cells[0].data
+
+fill_colors = glm.vec4(len(faces))
+fill_colors.rgba = 1,1,1,.85
+
+edge_colors = glm.vec4(len(faces))
+edge_colors.rgba = 0,0,0,1
+
+mesh = visual.Mesh(viewport, verts, faces,
+                   fill_colors, edge_colors, 0.5)
+
+# Render
+mesh.render(camera.transform)
+
+# Interaction with mouse (matplotlib backends only)
+if gsp.mode.startswith("matplotlib"):
+    camera.connect(viewport.axes, "motion",  mesh.render)
+    canvas.run()
+
---
+++ gsp/backend/matplotlib/visual/mesh.py
@@ -0,0 +1,109 @@
+# -----------------------------------------------------------------------------
+# Graphic Server Protocol (GSP) — matplotlib implementation
+# Copyright 2023 Vispy Development Team - BSD 2 Clauses licence
+# -----------------------------------------------------------------------------
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib.collections import PolyCollection
+from gsp.backend.matplotlib.core import Buffer
+from gsp.backend.matplotlib.transform import Mat4x4, Transform
+
+
+class Mesh:
+    def __init__(self, viewport, vertices, faces,
+                       fill_colors, edge_colors, edge_widths=0.5,
+                       mode = None):
+
+        self._viewport = viewport
+        self._mode = mode
+        self._vertices = vertices
+        self._faces = faces
+        self._fill_colors = fill_colors
+        self._edge_colors = edge_colors
+        self._edge_widths = edge_widths
+        self._collection = PolyCollection([], clip_on=False, snap=False)
+        self._viewport.axes.add_collection(self._collection, autolim=False)
+        self._transform = Mat4x4(np.zeros(16,np.float32))
+
+    def frontback(self, T):
+        """
+        Sort front and back facing triangles
+
+        Parameters:
+        -----------
+        T : (n,3) array
+
+           Triangles to sort
+
+        Returns:
+        --------
+        front and back facing triangles as (n1,3) and (n2,3) arrays (n1+n2=n)
+        """
+        Z = (T[:,1,0]-T[:,0,0])*(T[:,1,1]+T[:,0,1]) + \
+            (T[:,2,0]-T[:,1,0])*(T[:,2,1]+T[:,1,1]) + \
+            (T[:,0,0]-T[:,2,0])*(T[:,0,1]+T[:,2,1])
+        return Z < 0, Z >= 0
+
+
+    def render(self, transform):
+
+        # Update internal transform with given transform
+        self._transform.set_data(transform)
+
+        # Get vertices
+        if isinstance(self._vertices, Transform):
+            vertices = self._vertices.evaluate()
+        else:
+            vertices = np.asarray(self._vertices)
+        vertices = vertices.view(np.float32).reshape(-1,3)
+        
+        # Get faces
+        if isinstance(self._faces, Transform):
+            faces = self._faces.evaluate()
+        else:
+            faces = np.asarray(self._faces)
+        faces = faces.view(np.int64).reshape(-1,3)
+        
+        # Compute tranformed triangles (T) and their (mean) depth (Z)
+        T = self._transform(vertices)[faces]
+        Z = -T[:,:,2].mean(axis=1)
+
+        # Check which mode to use
+        index = None
+        if self._mode == "front":
+            index, _ = self.frontback(T)
+            T, Z = T[index], Z[index]
+        elif self._mode == "back":
+            _, index = self.frontback(T)
+            T, Z = T[index], Z[index]
+
+        # Fill colors
+        FC = self._fill_colors
+        if isinstance(FC, Transform):
+            FC = FC.evaluate({"depth": Z, "index": index})
+        elif index is not None:
+            FC = np.asarray(FC)[index]
+        else:
+            FC = np.asarray(FC)
+
+        # Edge colors
+        EC = self._edge_colors
+        if isinstance(EC, Transform):
+            EC = EC.evaluate({"depth": Z, "index": index})
+        elif index is not None:
+            EC = np.asarray(EC)[index]
+        else:
+            EC = np.asarray(EC)
+                
+        # Get 2d triangles
+        T = T[:,:,:2]
+        
+        # Sort triangles according to z buffer
+        I = np.argsort(Z)
+        
+        self._collection.set_verts(T[I,:])
+        self._collection.set_linewidths(0.5)
+        self._collection.set_facecolors(FC[I,:])
+        self._collection.set_edgecolors(EC[I,:])
+        self._collection.set_antialiased(True)
