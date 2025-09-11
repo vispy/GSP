feb1b2a50e57b4ed9016829149fc78a5593c66cc
Jerome Etienne <jerome.etienne@gmail.com>
2025-09-11 16:32:12 +0200

    basic cleaning of temporary test

--- examples/test-command-file-cycle.py
+++ examples/test-command-file-cycle.py
@@ -2,34 +2,39 @@ import gsp
 from gsp import core, visual, glm
 import numpy as np
 
+###########################################
 # Build a scene
+
 canvas = core.Canvas(512, 512, 100.0)
 viewport = core.Viewport(canvas, 0, 0, 512, 512, [1,1,1,1])
-n = 200_000
+n = 200
 P = glm.to_vec3(np.random.uniform(-1, +1, (n,2)), dtype=np.float32)
 pixels = visual.Pixels(positions=P, colors=[0,0,0,1])
 pixels.render(viewport)
 
+##########################################
 # Save a command file
+
 commands_filename = "test-command-file-cycle.command.json"
 gsp.save(filename=commands_filename)
 
-#########
+##########################################
 # Now reset everything and reload from command file, then run the queue
 # 
 
 # reset objects
 gsp.Object.objects = {}
 
-# load commands from file
+# load commands from file into a command queue
 command_queue = gsp.io.json.load(commands_filename)
 
 # KEY: REQUIRED FOR THE GLOBALS
 gsp.use("matplotlib")
 
+# Actually run the command queue
 command_queue.run(globals(), locals())
 
-#####################################
+##########################################
 # Finally display the result via matplotlib
 
 import matplotlib.pyplot as plt
